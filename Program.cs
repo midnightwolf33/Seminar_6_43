5b34e2781c5212a1254791eb859b512ec830d105
midnight33 <gra33@bk.ru>
2023-03-17 18:58:12 +0300

    Задача 43

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+double ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToDouble(Console.ReadLine() ?? "0");
+}
+
+double xVolume(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+
+double yVolume(double b1, double k1, double x)
+{
+    double y = k1 * x + b1;
+    return y;
+}
+
+double b1 = ReadData("Введите значение b1: ");
+double k1 = ReadData("Введите значение k1: ");
+double b2 = ReadData("Введите значение b2: ");
+double k2 = ReadData("Введите значение k2: ");
+
+double x = xVolume(b1, k1, b2, k2);
+double y = yVolume(b1, k1, x);
+
+Console.WriteLine("Значение Х = " + x);
+Console.WriteLine("Значение Y = " + y);
